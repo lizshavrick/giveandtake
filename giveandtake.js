72130c80d872233364ee4394449e52b37ffa6129
lizshavrick <shave343@newschool.edu>
2022-02-23 16:17:21 -0500

    hi

--- giveandtake.js
+++ giveandtake.js
@@ -1,4 +1,20 @@
-$('input').on('input', function() {
-    var v = $(this).val();
-    $('p').css('font-size', v + 'em')
-});
+
+
+document.querySelector('input').addEventListener('input', e => document.querySelector('h1').style.fontSize = e.target.value + 'em');
+
+document.querySelector('input').addEventListener('input', e => document.querySelector('p').style.fontSize = e.target.value + 'em');
+
+document.querySelector('input').addEventListener('input', e => document.querySelector('h2').style.fontSize = e.target.value + 'em');
+
+document.querySelector('input').addEventListener('input', e => document.querySelector('h3').style.fontSize = e.target.value + 'em');
+
+document.querySelector('input').addEventListener('input', e => document.querySelector('h4').style.fontSize = e.target.value + 'em');
+
+document.querySelector('input').addEventListener('input', e => document.querySelector('downloadbutton').style.fontSize = e.target.value + 'em');
+
+document.querySelector('input').addEventListener('input', e => document.querySelector('downloadbutton2').style.fontSize = e.target.value + 'em');
+
+document.querySelector('input').addEventListener('input', e => document.querySelector('downloadbutton3').style.fontSize = e.target.value + 'em');
+
+document.querySelector('input').addEventListener('input', e => document.querySelector('OpenSans-Regular').style.fontSize = e.target.value + 'em');
+
