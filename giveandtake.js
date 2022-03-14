04e528d1de3f4b2f0d147614f41b51af993fc4fa
lizshavrick <shave343@newschool.edu>
2022-03-13 23:43:47 -0400

    yuh

--- giveandtake.js
+++ giveandtake.js
@@ -1,20 +1,173 @@
 
 
-document.querySelector('input').addEventListener('input', e => document.querySelector('h1').style.fontSize = e.target.value + 'em');
+// document.querySelector('input').addEventListener('input', e => document.querySelector('h2').style.fontSize = e.target.value + 'em');
 
-document.querySelector('input').addEventListener('input', e => document.querySelector('p').style.fontSize = e.target.value + 'em');
+// document.querySelector('input').addEventListener('input', e => document.querySelector('h3').style.fontSize = e.target.value + 'em');
 
-document.querySelector('input').addEventListener('input', e => document.querySelector('h2').style.fontSize = e.target.value + 'em');
+// document.querySelector('input').addEventListener('input', e => document.querySelector('.ph1').style.fontSize = e.target.value + 'em');
 
-document.querySelector('input').addEventListener('input', e => document.querySelector('h3').style.fontSize = e.target.value + 'em');
+// document.querySelector('input').addEventListener('input', e => document.querySelector('.ph3').style.fontSize = e.target.value + 'em');
 
-document.querySelector('input').addEventListener('input', e => document.querySelector('h4').style.fontSize = e.target.value + 'em');
+// document.querySelector('input').addEventListener('input', e => document.querySelector('.ph4').style.fontSize = e.target.value + 'em');
 
-document.querySelector('input').addEventListener('input', e => document.querySelector('downloadbutton').style.fontSize = e.target.value + 'em');
+// document.querySelector('input').addEventListener('input', e => document.querySelector('.ph5').style.fontSize = e.target.value + 'em');
+// document.querySelector('input').addEventListener('input', e => document.querySelector('.ph6').style.fontSize = e.target.value + 'em');
 
-document.querySelector('input').addEventListener('input', e => document.querySelector('downloadbutton2').style.fontSize = e.target.value + 'em');
+// document.querySelector('input').addEventListener('input', e => document.querySelector('.ph6').style.fontSize = e.target.value + 'em');
 
-document.querySelector('input').addEventListener('input', e => document.querySelector('downloadbutton3').style.fontSize = e.target.value + 'em');
+// document.querySelector('input').addEventListener('input', e => document.querySelector('.ph7').style.fontSize = e.target.value + 'em');
 
-document.querySelector('input').addEventListener('input', e => document.querySelector('OpenSans-Regular').style.fontSize = e.target.value + 'em');
+// document.querySelector('input').addEventListener('input', e => document.querySelector('.ph8').style.fontSize = e.target.value + 'em');
+// document.querySelector('input').addEventListener('input', e => document.querySelector('.ph9').style.fontSize = e.target.value + 'em');
+
+// document.querySelector('input').addEventListener('input', e => document.querySelector('.shape1').style.fontSize = e.target.value + 'em');
+
+// document.querySelector('input').addEventListener('input', e => document.querySelector('background-02.svg').style.fontSize = e.target.value + 'em');
+
+
+$( function() {
+    $( ".scrap-buttonhome" ).draggable();
+  } );
+
+$( function() {
+    $( ".template-buttonhome" ).draggable();
+  } );
+
+$( function() {
+    $( ".material-buttonhome" ).draggable();
+  } );
+
+
+$( function() {
+    $( ".material-buttont" ).draggable();
+  } );
+
+$( function() {
+    $( ".scrap-buttont" ).draggable();
+  } );
+
+$( function() {
+    $( ".template-buttont" ).draggable();
+  } );
+
+$( function() {
+    $( ".material-buttont" ).draggable();
+  } );
+
+$( function() {
+    $( ".scrap-buttont" ).draggable();
+  } );
+
+$( function() {
+    $( ".template-buttont" ).draggable();
+  } );
+
+$( function() {
+    $( ".material-buttonm" ).draggable();
+  } );
+
+$( function() {
+    $( ".scrap-buttonm" ).draggable();
+  } );
+
+$( function() {
+    $( ".template-buttonm" ).draggable();
+  } );
+
+$( function() {
+    $( ".material-buttons" ).draggable();
+  } );
+
+$( function() {
+    $( ".scrap-buttons" ).draggable();
+  } );
+
+$( function() {
+    $( ".template-buttons" ).draggable();
+  } );
+
+$( function() {
+    $( ".material-buttont2" ).draggable();
+  } );
+
+$( function() {
+    $( ".scrap-buttontactive" ).draggable();
+  } );
+
+$( function() {
+    $( ".template-buttont" ).draggable();
+  } );
+
+$( function() {
+    $( ".m-resource-button0" ).draggable();
+  } );
+
+$( function() {
+    $( ".m-resource-button1" ).draggable();
+  } );
+
+$( function() {
+    $( ".m-resource-button2" ).draggable();
+  } );
+
+$( function() {
+    $( ".m-resource-button3" ).draggable();
+  } );
+
+$( function() {
+    $( ".m-resource-button4" ).draggable();
+  } );
+
+$( function() {
+    $( ".m-resource-button5" ).draggable();
+  } );
+
+$( function() {
+    $( ".m-resource-button0" ).draggable();
+  } );
+
+$( function() {
+    $( ".m-resource-button6" ).draggable();
+  } );
+
+$( function() {
+    $( ".m-resource-button7" ).draggable();
+  } );
+
+$( function() {
+    $( ".m-resource-button8" ).draggable();
+  } );
+
+$( function() {
+    $( ".m-resource-button9" ).draggable();
+  } );
+
+$( function() {
+    $( ".m-resource-button10" ).draggable();
+  } );
+
+$( function() {
+    $( ".m-resource-button11" ).draggable();
+  } );
+
+$( function() {
+    $( ".m-resource-button16" ).draggable();
+  } );
+$( function() {
+    $( ".m-resource-button13" ).draggable();
+  } );
+
+$( function() {
+    $( ".m-resource-button14" ).draggable();
+  } );
+
+$( function() {
+    $( ".m-resource-button15" ).draggable();
+  } );
+
+$( function() {
+    $( ".m-resource-button15" ).draggable();
+  } );
+
+
 
